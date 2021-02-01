6649584ab0822c944a7f19c549e111bceec65b42
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-02-01 17:12:50 +0900

    velopert react intro

---
+++ hello-react/src/App.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  const name = "리액트";
+
+  return (
+    <div>
+      <div className="react">{name}</div>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다</p>
+    </div>
+  );
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <p>
+  //           Edit <code>src/App.js</code> and save to reload.
+  //         </p>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //       </header>
+  //     </div>
+  //   );
+  // }
+}
+export default App;
