a96fdc1c4c74cb1f0503fb019c970e923d328a0d
G_Polya <wlskaka4@gmail.com>
2021-02-02 16:32:39 +0900

    class component state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,36 +1,8 @@
-import React, { Fragment } from "react";
-// import logo from './logo.svg';
-import "./App.css";
+import React from "react"
+import Counter from "./Counter"
 
-function App() {
-  const name = "리액트";
-
-  return (
-    <div>
-      <div className="react">{name}</div>
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다</p>
-    </div>
-  );
-  //   return (
-  //     <div className="App">
-  //       <header className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <p>
-  //           Edit <code>src/App.js</code> and save to reload.
-  //         </p>
-  //         <a
-  //           className="App-link"
-  //           href="https://reactjs.org"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           Learn React
-  //         </a>
-  //       </header>
-  //     </div>
-  //   );
-  // }
+const App = ()=>{
+  return <Counter />
 }
+
 export default App;
