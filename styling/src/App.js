82b71baf104e38f6c2809084617da30703f4ac9c
G_Polya <wlskaka4@gmail.com>
2021-02-13 21:10:09 +0900

    왜 안되냐 시발

---
+++ styling/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StyledComponent from './StyledComponent';
+
+function App() {
+  return (
+    <div >
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
