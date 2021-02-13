83e140ada70aa681c7d7dfb0527a2a8eaf441d84
G_Polya <wlskaka4@gmail.com>
2021-02-13 20:28:39 +0900

    styling

---
+++ 9. styling-react/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import logo from "./logo.svg"
+import "./App.css"
+import SassCompnent from "./SassCompnent";
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <SassCompnent />
+      </div>
+    )
+  }
+}
+
+export default App;
