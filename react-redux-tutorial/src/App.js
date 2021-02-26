cfa4c4a524faaaa0171102ce8151086eb6ef2570
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-02-26 10:58:08 +0900

    react-redux turtorial finished

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Todos from './components/Todos';
 import CounterContainer from './containers/CounterContainer';
-import TodosContainer from './containers/TodosContainer';
+import TodosContainer from './containers/TodoContainer';
 const App = () => {
   return (
     <div>
