89ac8390e83009dc44ada31fc2bddf0b732899f0
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-02-26 11:54:22 +0900

    redux thunk intro

---
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -0,0 +1,17 @@
+import React, { useCallback } from "react";
+import { connect, useSelector, useDispatch } from "react-redux";
+import { increase, decrease } from "../modules/counter";
+import Counter from "../components/Counter";
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+
+export default CounterContainer;
