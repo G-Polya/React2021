f0fe999b247fcfaff92ad42c6a417fa8316b5827
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-03-08 14:32:00 +0900

    server side rendering intro

--- server-side-rendering/src/App.js
+++ server-side-rendering/src/App.js
@@ -4,15 +4,15 @@ import Menu from "./components/Menu";
 import RedPage from "./pages/RedPage";
 import BluePage from "./pages/BluePage";
 
-const App = () => {
+function App() {
   return (
     <div>
       <Menu />
       <hr />
-      <Route path="red" component={RedPage} />
-      <Route path="blue" component={BluePage} />
+      <Route path="/red" component={RedPage} />
+      <Route path="/blue" component={BluePage} />
     </div>
   );
-};
+}
 
 export default App;
