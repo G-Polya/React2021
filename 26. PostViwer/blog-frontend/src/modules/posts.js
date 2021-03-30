1de6a6b214084563441b2f0afccb4f17e5cc5604
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-03-30 17:51:33 +0900

    pagination

--- 26. PostViwer/blog-frontend/src/modules/posts.js
+++ 26. PostViwer/blog-frontend/src/modules/posts.js
@@ -20,16 +20,19 @@ const listPostsSaga = createRequestSaga(LIST_POSTS, postsAPI.listPosts);
 export function* postsSaga() {
   yield takeLatest(LIST_POSTS, listPostsSaga);
 }
+
 const initialState = {
   posts: null,
   error: null,
+  lastPage: 1,
 };
 
 const posts = handleActions(
   {
-    [LIST_POSTS_SUCCESS]: (state, { payload: posts }) => ({
+    [LIST_POSTS_SUCCESS]: (state, { payload: posts, meta: response }) => ({
       ...state,
       posts,
+      lastPage: parseInt(response.headers['last-page'], 10), // 문자열을 숫자로 변환
     }),
     [LIST_POSTS_FAILURE]: (state, { payload: error }) => ({
       ...state,
